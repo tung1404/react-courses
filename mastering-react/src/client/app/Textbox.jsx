cfd4c710fe8230807abeb3977b74355cf0ae82b5
Thanh Tùng <tungmiho@gmail.com>
2017-04-03 17:41:24 +0700

    no message

--- mastering-react/src/client/app/Textbox.jsx
+++ mastering-react/src/client/app/Textbox.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
+var GlyphIcon = React.createClass({
+  render: function() {
+    return (
+      <span className={'glyphicon glyphicon-'+ this.props.icon}></span>
+    );
+  }
+});
+
+var ReactMixin1 = {
+  update: function() {
+    console.log('update ReactMixin1 ');
+  },
+  componentWillMount: function() {
+    console.log('componentWillMount ReactMixin1 ');
+  }
+};
+
+var ReactMixin2 = {
+  update: function() {
+    console.log('update ReactMixin2 ');
+  },
+  componentWillMount: function() {
+    console.log('componentWillMount ReactMixin2 ');
+  },
+};
+
 var Textbox = React.createClass({
+  mixins: [ReactMixin2, ReactMixin1],
   getInitialState: function() {
     return {isEditing: false};
   },
   update: function() {
+    console.log('update Textbox ');
     this.props.update(this.refs.messageTextbox.value);
     this.setState({isEditing: false});
   },
@@ -19,9 +47,9 @@ var Textbox = React.createClass({
         <input type="text" disabled={!this.state.isEditing} ref="messageTextbox"/>
         {
           this.state.isEditing ?
-            <button onClick={this.update}>Update</button>
+            <button className={'btn btn-default'} onClick={this.update}><GlyphIcon icon='ok'/>Update</button>
             :
-            <button onClick={this.edit}>Edit</button>
+            <button className={'btn btn-default'} onClick={this.edit}><GlyphIcon icon='pencil'/>Edit</button>
         }
       </div>
     );
